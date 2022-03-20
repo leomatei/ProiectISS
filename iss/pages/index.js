9a67241675d2a0a91c723ae2a36370cabbfd5e3f
Matei Leonard <leonard.matei2011@yahoo.com>
2022-03-21 00:11:25 +0200

    initial commit, working with graphcms

--- iss/pages/index.js
+++ iss/pages/index.js
@@ -1,4 +1,41 @@
-const Home=()=> {
+import {gql, GraphQLClient} from 'graphql-request'
+export const getStaticProps = async () =>{
+  
+  const url = process.env.NEXT_PUBLIC_GRAPH_CMS_ENDPOINT
+  const graphQLClient=new GraphQLClient (url, {
+    headers:{
+      "Authorizations": process.env.NEXT_PUBLIC_GRAPH_CMS_TOKEN,
+    }
+  })
+  const query=gql`
+  query{
+    products{
+      id,
+      productName,
+      productPrice,
+      productStock,
+      productDescription,
+      slug,
+      productPhoto{
+        fileName,
+        url
+      }
+    }
+  }`
+
+  const data=await graphQLClient.request(query)
+  const products=data.products
+  return {
+    props:{
+      products,
+    }
+  }
+}
+
+
+
+const Home=({products})=> {
+  console.log(products)
   return (
     <div>
       Heloo
